6410281f05431c787fdb6ac8c6ddc8d6f06aa1aa
ProjetL3Info <ilyas.off.script@gmail.com>
2023-03-09 21:41:08 +0100

    v0.9

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,11 +8,11 @@ const public_users = express.Router();
 public_users.post("/register", (req,res) => {
   const {username,password}=req.body
   if(!username || !password)
-    return res.status(404).json({message: "Missing username and/or password"});    
+    return res.status(403).json({message: "Missing username and/or password."});    
   if(!isValid(username))
-    return res.status(404).json({message: `User ${username} already exists!`});    
+    return res.status(403).json({message: `User ${username} already exists!`});    
   users.push({username,password})
-  return res.status(200).json({message: `User ${username} successfully registered`});
+  return res.status(200).json({message: `User ${username} successfully registered.`});
 });
 
 // Get the book list available in the shop
@@ -24,13 +24,13 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   const {isbn} = req.params
   if(!books[isbn])
-    return res.status(404).json({message:`Book with ISBN ${isbn} not found`})
+    return res.status(404).json({message:`Book with ISBN ${isbn} not found.`})
   return res.status(200).json(JSON.stringify(books[isbn],null,2));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const {auhor:athorName} = req.params
+  const {author:athorName} = req.params
   const author=athorName.toLowerCase()
   const authorBooks= []
   Object.keys(books).forEach(isbn=>{
@@ -38,7 +38,7 @@ public_users.get('/author/:author',function (req, res) {
       authorBooks.push({isbn,...books[isbn]})
   })
   if(!authorBooks.length)
-    return res.status(200).json({message:`No books from author "${athorName}" were found`})
+    return res.status(200).json({message:`No books from author "${athorName}" were found.`})
   return res.status(200).json(JSON.stringify(authorBooks,null,2));
 });
 
@@ -52,7 +52,7 @@ public_users.get('/title/:title',function (req, res) {
     correspondingBooks.push({isbn,...books[isbn]})
   })
   if(!correspondingBooks.length)
-    return res.status(200).json({message:`No books with title "${bookTitle}" were found`})
+    return res.status(200).json({message:`No books with title "${bookTitle}" were found.`})
   return res.status(200).json(JSON.stringify(correspondingBooks,null,2));
 });
 
@@ -60,9 +60,9 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   const {isbn} = req.params
   if(!books[isbn])
-      return res.status(404).json({message:`Book with ISBN ${isbn} not found`})
-  if(!Object.keys(books[isbn]).length)
-      return res.status(200).json(JSON.stringify({message:`No reviews for the book with ISBN ${isbn}`,reviews:books[isbn].reviews},null,2));
+      return res.status(404).json({message:`Book with ISBN ${isbn} not found.`})
+  if(!Object.keys(books[isbn].reviews).length)
+      return res.status(200).json(JSON.stringify({message:`No reviews for the book with ISBN ${isbn}.`,reviews:books[isbn].reviews},null,2));
 
   return res.status(200).json(JSON.stringify(books[isbn].reviews,null,2));
  });
